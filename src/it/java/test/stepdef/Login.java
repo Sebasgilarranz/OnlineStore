76bed4e3b562fa5bfd25856b55a157759f06e053
Sebasgilarranz <32475010+Sebasgilarranz@users.noreply.github.com>
2021-12-08 12:08:40 -0500

    Update Login.java

--- src/it/java/test/stepdef/Login.java
+++ src/it/java/test/stepdef/Login.java
@@ -26,7 +26,7 @@ public class Login{
 	
 	@Given("^User is on the home page$")
 	public void user_is_on_the_home_page() throws Throwable {
-		driver.get("http://www.store.demoqa.com");
+		driver.get("http://www.google.com");
 		Assert.assertEquals(0, driver.findElements(By.id("account_logout")).size());
 	}
 
